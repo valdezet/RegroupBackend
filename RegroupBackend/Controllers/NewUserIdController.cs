f0c8f3420cb81ab116234a29637d2f86dac4a2b2
Erard Valdez <valdez.erardtimothy@gmail.com>
2023-05-31 06:03:49 +0800

    give new user GUID

---
+++ RegroupBackend/Controllers/NewUserIdController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RegroupBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewUserIdController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
